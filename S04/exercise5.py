cfe27e5d97af98e6f1faa62486624517cccaa742
cristinamartinpantoja <c.martinpan05@gmail.com>
2024-02-06 16:43:26 +0100

    This is going to be my first but not last commit

--- S04/exercise5.py
+++ S04/exercise5.py
@@ -3,11 +3,18 @@ from pathlib import Path
 FILENAME = "sequences/ADA"
 file_contents = Path(FILENAME).read_text()
 
+list_contents = file_contents.split("\n")
+list_contents.pop(0)
+
+print(len("".join(list_contents)))
+
+#another option
+
+list_contents = file_contents.split("\n")
+index = file_contents.find("\n")
+file_contents = (file_contents[index:]).replace
 
 
 
 
 
-
-token:
-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
