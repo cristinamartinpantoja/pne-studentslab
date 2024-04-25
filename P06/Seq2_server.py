2608b0974236b0d37007b08364ee037e40ed65ad
cristinamartinpantoja <c.martinpan05@gmail.com>
2024-04-25 17:42:04 +0200

    This is going to be my first but not last commit

--- P06/Seq2_server.py
+++ P06/Seq2_server.py
@@ -2,111 +2,95 @@ import http.server
 import socketserver
 import termcolor
 from pathlib import Path
-from Seq0 import Seq
-import os
+import jinja2 as j
+from urllib.parse import parse_qs, urlparse
 
-# Define the Server's port
+def read_html_file(filename):
+    contents = Path("html/" + filename).read_text()
+    contents = j.Template(contents)
+    return contents
+
 PORT = 8080
 
-# -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
 
-# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inherits all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        """This method is called whenever the client invokes the GET method
-        in the HTTP protocol request"""
-        seq = ""
-        s = Seq(seq)
-        # Print the request line
+        url_path = urlparse(self.path)
+        path = url_path.path
+        arguments = parse_qs(url_path.query)
+
+        GENES = ["U5", "ADA", "FRAT1", "RNU6_269P", "FXN"]
         termcolor.cprint(self.requestline, 'green')
-
-        # IN this simple server version:
-        # We are NOT processing the client's request
-        # It is a happy server: It always returns a message saying
-        # that everything is ok
-        import jinja2 as j
-        def read_html_file(filename):
-            contents = Path("html/" + filename).read_text()
-            contents = j.Template(contents)
-            return contents
-
-        # Message to send back to the client
-        if self.requestline.startswith("GET / ") or self.requestline.startswith("GET /index"):
-            contents = Path("../P06/html/index.html").read_text()
-        elif self.requestline.startswith("GET /ping?"):
-            contents = Path("../P06/html/ping.html").read_text()
-
-        elif self.requestline.startswith("GET /actions?sequences"):
-            dict = {"0": "ACGTAGTCAGTAGTAGCTAGC", "1": "GATCCCAGTTAAGA", "2": "ACTGTGGGCATATAATCAGCGAGCA", "3": "TCAGTAGCTAGCTAGCTAGTCAGCTAGC", "4": "CGGCTAGCTAGCTAGTCGATCGATCGATGCA"}
-            num = self.requestline[23:]
-            num = num[0]
-            if num in dict:
-                sequence = dict[num]
-                contents = read_html_file("sequences.html").render(context={"name": num, "todisplay": sequence})
-
-        elif self.requestline.startswith("GET /actions?genes"):
-            list = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
-            name = self.requestline[19:]
-            name = name[:-9]
-            if name in list:
-                text = s.read_fasta(f"../Sequences/{name}.txt")
-                contents = read_html_file("genes.html").render(context={"name": name, "todisplay": text})
-
-        elif self.requestline.startswith("GET /actions?msg"):
-            new = self.requestline.split("&")
-            msg = new[0]
-            seq = msg[17:]
-            op = new[1]
-            op = op[9:-9]
-            result = ""
-            s = Seq(seq)
-            if op == "info":
-                result = f"""Length of sequence: {str(s.length())}<br>
-A: {s.count("A")}<br>
-C: {s.count("C")}<br>
-T: {s.count("T")}<br>
-G: {s.count("G")}"""
-            elif op == "comp":
-                result = s.comp()
-            elif op == "rev":
-                result = s.rev()
-            contents = read_html_file("operation.html").render(context={"sequence": seq, "operation": op, "results": result})
+        if self.path == "/":
+            contents = Path('html/index.html').read_text()
+        elif self.path[:-1] == "/ping":
+            contents = Path("html/ping.html").read_text()
+        elif self.path[:4] == "/get":
+            for gene in GENES:
+                index = GENES.index(gene)
+                if 0 <= index <= 4:
+                    if self.path.endswith(f"{index}"):
+                        sequence = Path("..", "sequences", gene).read_text().splitlines()
+                        sequence = sequence[1:]
+                        msg = ""
+                        for line in sequence:
+                            msg += line
+                        contents = read_html_file("get.html").render(context={"todisplay": str(msg), "sequence": str(index)})
+        elif self.path[:5] == "/gene":
+            for gene in GENES:
+                if self.path.endswith(f"{gene}"):
+                    sequence = Path("..", "sequences", gene).read_text().splitlines()
+                    sequence = sequence[1:]
+                    msg = ""
+                    for line in sequence:
+                        msg += line
+                    contents = read_html_file("gene.html").render(context={"todisplay": str(msg), "gene": str(gene)})
+        elif self.path.startswith("/operation"):
+            msg = arguments['msg'][0]
+            if self.path.endswith("Info"):
+                total_len = len(msg)
+                a_a = f"A: {msg.count('A')} ({((msg.count('A') / total_len) * 100):.1f}%)"
+                a_c = f"C: {msg.count('C')} ({((msg.count('C') / total_len) * 100):.1f}%)"
+                a_g = f"G: {msg.count('G')} ({((msg.count('G') / total_len) * 100):.1f}%)"
+                a_t = f"T: {msg.count('T')} ({((msg.count('T') / total_len) * 100):.1f}%)"
+                info_msg = f"Total length: {len(msg)}\n {a_a}\n {a_c}\n {a_g}\n {a_t}"
+                contents = read_html_file("operation.html").render(context={"result": info_msg, "operation": "info", "sequence": msg})
+            elif self.path.endswith("Comp"):
+                complement = ""
+                for base in msg:
+                        if base == "A":
+                            complement += "T"
+                        elif base == "G":
+                            complement += "C"
+                        elif base == "C":
+                            complement += "G"
+                        elif base == "T":
+                            complement += "A"
+                contents = read_html_file("operation.html").render(context={"result": complement, "operation": "comp", "sequence": msg})
+            elif self.path.endswith("Rev"):
+                seq_n = msg[:len(msg)]
+                reverse = seq_n[::-1]
+                contents = read_html_file("operation.html").render(context={"result": reverse, "operation": "rev", "sequence": msg})
 
         else:
-            contents = Path("../S14/error.html").read_text()
+            contents = Path("html/error.html").read_text()
+            self.send_response(404)
 
-
-        # Generating the response message
-        self.send_response(200)  # -- Status line: OK!
-
-        # Define the content-type header:
-        self.send_header('Content-Type', 'html')
-        self.send_header('Content-Length', len(contents.encode()))
-
-        # The header is finished
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
         self.end_headers()
-
-        # Send the response message
-        self.wfile.write(contents.encode())
-
+        self.wfile.write(str.encode(contents))
         return
 
-# ------------------------
-# - Server MAIN program
-# ------------------------
-# -- Set the new handler
 Handler = TestHandler
 
-# -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
 
     print("Serving at PORT", PORT)
 
-    # -- Main loop: Attend the client. Whenever there is a new
-    # -- clint, the handler is called
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
