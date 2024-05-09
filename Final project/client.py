bff53a2f7e28b5b2be0f61282b7a5c45e66347b0
cristinamartinpantoja <c.martinpan05@gmail.com>
2024-05-09 19:02:19 +0200

    This is going to be my first but not last commit

--- Final project/client.py
+++ Final project/client.py
@@ -3,19 +3,19 @@ import termcolor
 from Seq2 import Seq
 class Client:
     def __init__(self, IP, PORT):
-        self.ip = IP
-        self.port = PORT
+        self.IP = IP
+        self.PORT = PORT
         pass
 
     def __str__(self):
-        return f"Connection to SERVER at {self.ip}, PORT: {self.port}"
+        return f"Connection to SERVER at {self.IP}, PORT: {self.PORT}"
 
     def ping(self):
-        print(termcolor.colored("PING Command!", "green"))
+        print(termcolor.colored("Ping Command!", "blue"))
 
     def talk(self, msg):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.ip, self.port))
+        s.connect((self.IP, self.PORT))
         s.send(str.encode(msg))
         response = s.recv(2048).decode("utf-8")
         s.close()
